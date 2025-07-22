0905dfc5a1b81164c5318dcb6e84ff1eda7245e5
wrtx-dev <wrtx.dev@outlook.com>
2025-07-22 22:46:37 +0800

    添加 HTML 预览功能，更新国际化文件以支持复制和预览消息

--- src/renderer/src/components/markdown/codeblock.tsx
+++ src/renderer/src/components/markdown/codeblock.tsx
@@ -1,6 +1,9 @@
-import { FC, memo } from "react";
+import { FC, memo, useRef } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { HtmlPreviewer } from "./htmlPreviewer";
+import { useTranslation } from "react-i18next";
+import { Copy, Fullscreen } from "lucide-react";
 
 
 interface CodeBlockProps {
@@ -20,6 +23,8 @@ const CODE_STYLE = {
 };
 
 const CodeBlockComponent: FC<CodeBlockProps> = ({ language, value }) => {
+    const ref = useRef<any>(null);
+    const { t } = useTranslation();
     return (
         <div className="w-full overflow-y-hidden">
             <SyntaxHighlighter
@@ -36,13 +41,30 @@ const CodeBlockComponent: FC<CodeBlockProps> = ({ language, value }) => {
                 <span className="text-xs font-semibold font-serif text-emerald-500">
                     {language}
                 </span>
-                <button
-                    className="btn btn-ghost btn-xs border-none shadow-none focus-visible:shadow-none focus-visible:border-none focus-visible:bg-transparent hover:bg-transparent hover:text-white focus-visible:ring-0"
-                    onClick={() => navigator.clipboard.writeText(value)}
-                >
-                    copy
-                </button>
+                <div className="inline-flex flex-row items-center gap-1.5">
+                    <button
+                        className="border-none shadow-none focus-visible:shadow-none focus-visible:border-none focus-visible:bg-transparent hover:bg-transparent hover:text-white focus-visible:ring-0 inline-flex flex-row items-center gap-1 text-xs"
+                        onClick={() => navigator.clipboard.writeText(value)}
+                    >
+                        <Copy strokeWidth={1} className="size-3" />
+                        {t("message.copy")}
+                    </button>
+                    {language.toLowerCase() === "html" &&
+                        <button
+                            className="border-none shadow-none focus-visible:shadow-none focus-visible:border-none focus-visible:bg-transparent hover:bg-transparent hover:text-white focus-visible:ring-0 inline-flex flex-row items-center gap-1 text-xs"
+                            onClick={() => {
+                                if (ref && ref.current) {
+                                    ref.current.open();
+                                }
+                            }}
+                        >
+                            <Fullscreen strokeWidth={1} className="size-3" />
+                            {t("message.preview")}
+                        </button>
+                    }
+                </div>
             </div>
+            {language.toLowerCase() === "html" && <HtmlPreviewer content={value} ref={ref} />}
         </div>
     );
 };
---
+++ src/renderer/src/components/markdown/htmlPreviewer.tsx
@@ -0,0 +1,46 @@
+import { forwardRef, Ref, useImperativeHandle, useState } from "react";
+import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
+
+interface HtmlPreviewerProps {
+    content: string;
+}
+
+const HtmlPrivewDialog = forwardRef(({ content }: HtmlPreviewerProps, ref: Ref<any>) => {
+    const [openDialog, setOpenDialog] = useState(false);
+    useImperativeHandle(ref, () => (
+        {
+            open: () => {
+                setOpenDialog(true);
+            },
+            close: () => {
+                setOpenDialog(false);
+            },
+            isOpen: () => openDialog
+        }
+    ))
+    return (
+        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+            <DialogContent className="min-w-[90vw] lg:min-w-[80vw] overflow-y-auto h-[90vh] px-0.5 py-1.5" onEscapeKeyDown={() => setOpenDialog(false)}>
+                <DialogTitle className="text-sm font-sans w-full hidden">
+                    preview HTML content
+                </DialogTitle>
+                <DialogDescription className="hidden">preview html content</DialogDescription>
+                <div className="w-full h-full overflow-y-auto">
+                    <iframe
+                        className="w-full h-full"
+                        srcDoc={content}
+                        title="HTML Preview"
+                        sandbox="allow-scripts"
+                    />
+                </div>
+            </DialogContent>
+
+        </Dialog>
+    );
+});
+
+HtmlPrivewDialog.displayName = "HtmlPreviewer";
+
+export const HtmlPreviewer = HtmlPrivewDialog;
