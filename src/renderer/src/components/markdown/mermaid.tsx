0264387c556d3469fa7ea5f544faafe5918a5da6
wrtx-dev <wrtx.dev@outlook.com>
2025-07-23 00:47:42 +0800

    添加 Mermaid 组件的 SVG 预览功能，优化代码结构，修复渲染错误处理

--- src/renderer/src/components/markdown/mermaid.tsx
+++ src/renderer/src/components/markdown/mermaid.tsx
@@ -1,8 +1,10 @@
 import mermaid from "mermaid";
-import { useEffect, useState } from "react";
+import { forwardRef, Ref, useEffect, useImperativeHandle, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { CodeBlock } from "./codeblock";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "../ui/dialog";
+import { Fullscreen } from "lucide-react";
 
 export default function Mermaid({ content, loading }: { content: string, loading?: boolean }) {
     const { t } = useTranslation();
@@ -43,39 +45,78 @@ export default function Mermaid({ content, loading }: { content: string, loading
         const timer = setTimeout(renderMermaid, 100);
         return () => clearTimeout(timer);
     }, [content, lastRenderedCode, loading]);
-
+    const ref = useRef<any>(null);
 
     return (
-        <>
-            <div className="w-full">
-                <Tabs defaultValue={"graph"}>
-                    <TabsList className="text-xs h-6 rounded-xs bg-transparent gap-0.5">
-                        <TabsTrigger value="graph" className="rounded-xs data-[state=inactive]:text-white data-[state=inactive]:bg-white/20">{t('graph')}</TabsTrigger>
-                        <TabsTrigger value="code" className="rounded-xs data-[state=inactive]:text-white data-[state=inactive]:bg-white/20">{t('code')}</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="code">
-                        <CodeBlock
-                            language="mermaid"
-                            value={content}
-                        />
-                    </TabsContent>
-                    <TabsContent value="graph">
-                        {loading === true &&
-                            <div className="w-full h-16 flex flex-row justify-center items-center text-lg text-red-400">
-                                {t('renderingGraph')}
-                                <div className="border rounded-full size-6 border-t-red-500 animate-spin" />
-                            </div>}
-                        {!error ?
+        <div className="w-full">
+            <Tabs defaultValue={"graph"}>
+                <TabsList className="text-xs h-6 rounded-xs bg-transparent gap-0.5">
+                    <TabsTrigger value="graph" className="rounded-xs data-[state=inactive]:text-white data-[state=inactive]:bg-white/20">{t('graph')}</TabsTrigger>
+                    <TabsTrigger value="code" className="rounded-xs data-[state=inactive]:text-white data-[state=inactive]:bg-white/20">{t('code')}</TabsTrigger>
+                </TabsList>
+                <TabsContent value="code">
+                    <CodeBlock
+                        language="mermaid"
+                        value={content}
+                    />
+                </TabsContent>
+                <TabsContent value="graph">
+                    {loading === true &&
+                        <div className="w-full h-16 flex flex-row justify-center items-center text-lg text-red-400">
+                            {t('renderingGraph')}
+                            <div className="border rounded-full size-6 border-t-red-500 animate-spin" />
+                        </div>}
+                    {!error ?
+                        <div className="w-full h-full flex flex-col">
                             <div
                                 className="mermaid text-xs"
                                 dangerouslySetInnerHTML={{ __html: svg }}
-                            /> : <div className="w-full h-16 flex flex-col justify-center items-center text-lg text-red-400">
-                                {t('mermaidSyntaxError')}
+                            />
+                            <div className="flex flex-row justify-end items-center gap-2 mt-2">
+                                <button
+                                    className="border-none shadow-none focus-visible:shadow-none focus-visible:border-none focus-visible:bg-transparent hover:bg-transparent hover:text-white focus-visible:ring-0 inline-flex flex-row items-center gap-1 text-xs"
+                                    onClick={() => {
+                                        if (ref && ref.current) {
+                                            ref.current.open();
+                                        }
+                                    }}
+                                >   <Fullscreen strokeWidth={1} className="size-3" />
+                                    <span className="text-xs">{t("message.preview")}</span>
+                                </button>
                             </div>
-                        }
-                    </TabsContent>
-                </Tabs>
-            </div>
-        </>
+                        </div> : <div className="w-full h-16 flex flex-col justify-center items-center text-lg text-red-400">
+                            {t('mermaidSyntaxError')}
+                        </div>
+                    }
+                </TabsContent>
+            </Tabs>
+            {!loading && !error && svg && <SvgPreviewer svg={svg} ref={ref} />}
+        </div>
+
     );
 }
+
+
+const SvgPreviewer = forwardRef(({ svg }: { svg: string }, ref: Ref<any>) => {
+    const [open, setOpen] = useState(false);
+    useImperativeHandle(ref, () => ({
+        open: () => {
+            setOpen(true);
+        },
+        close: () => {
+            setOpen(false);
+        },
+        isOpen: () => open
+    }));
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="min-w-[90vw] lg:min-w-[80vw] overflow-y-auto h-[90vh] px-0.5 py-1.5 bg-gray-800/95" onEscapeKeyDown={() => setOpen(false)}>
+                <DialogTitle className="text-sm font-sans w-full h-6 select-none cursor-default hidden">
+                    preview mermaid graph
+                </DialogTitle>
+                <DialogDescription className="hidden">preview mermaid graph</DialogDescription>
+                <div className="mermaid text-xs overflow-y-auto" dangerouslySetInnerHTML={{ __html: svg }} />
+            </DialogContent>
+        </Dialog>
+    )
+});
